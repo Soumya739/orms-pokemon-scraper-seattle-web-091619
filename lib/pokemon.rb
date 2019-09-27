0498a37739724f275ae12a5983a051ac828bc1b7
Soumya_Goyal <somyagoyal739@gmail.com>
2019-09-27 10:55:02 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,29 +1,24 @@
+require "pry"
 class Pokemon
     attr_accessor :name, :type, :db, :id
     
-    def initialize (params = {name: name, type: type,  db: @db, id: id=nil})
-        @id = params[:id]
-            @name = params[:name]
-            @type = params[:type]
-            @db = params[:db]
-    end
-
-    
-    def create_table
-        sql = "CREATE TABLE pokemon (id INTEGER PRIMARY KEY, name TEXT, type TEXT)"    
-        @db[:conn].execute(sql)
+    def initialize (poke = {name: name, type: type,  db: db, id: id=nil})
+        @id = poke[:id]
+        @name = poke[:name]
+        @type = poke[:type]
+        @db = poke[:db]
     end
 
     def self.save(name, type, db)
-            sql = "INSERT INTO pokemon (name, type) VALUES(?, ?)"
-            # @id = @db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-            db.execute(sql, name, type)
+        sql = "INSERT INTO pokemon (name, type) VALUES(?, ?)"
+        # @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+        db.execute(sql, name, type)
     end
 
     def self.find(id, db)
        sql = "SELECT * FROM pokemon WHERE id = ? "
        abc = db.execute(sql,id)[0]
        puts abc.class
-        ab = Pokemon.new({name: abc[1], type: abc[2], db: db, id: abc[0]})
+       Pokemon.new({id: abc[0], name: abc[1], type: abc[2], db: db})
     end
  end
